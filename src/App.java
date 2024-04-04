d203031ea44ebcfeb150c8cc8296f78e4e98fc55
GalaxyAYL <josealbertoayalazumba@gmail.com>
2024-04-04 16:55:48 -0500

    final

--- src/App.java
+++ src/App.java
@@ -1,57 +1,45 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        /*
-         * Crear una instancia de la clase Repaso.
-         * 
-         * Una 'instancia' es un objeto concreto creado a partir de una clase.
-         * En este caso, estamos creando un nuevo objeto llamado 'dataStructuresPrep' a
-         * partir
-         * de la clase 'Repaso'. Cada instancia de una clase tiene sus propios
-         * atributos y métodos, lo que significa que podemos usar 'dataStructuresPrep'
-         * para
-         * acceder a los métodos definidos dentro de la clase 'Repaso'.
-         */
+
         DataStructuresPrep dataStructuresPrep = new DataStructuresPrep();
 
-        /*
-         * Usar la instancia 'dataStructuresPrep' para llamar al método
-         * 'subtractWithoutSubtracting'.
-         * 
-         * Ahora que tenemos una instancia de 'Repaso', podemos usarla para llamar a sus
-         * métodos.
-         * Aquí, llamamos al método 'subtractWithoutSubtracting', pasando 15 y 5 como
-         * argumentos.
-         * Este método está diseñado para restar el segundo número del primero sin usar
-         * el
-         * operador de resta tradicional. Es un buen ejercicio para entender cómo
-         * podemos
-         * realizar operaciones básicas de manera creativa en programación.
-         */
+        //SUBSTRACCION
         int resultado = dataStructuresPrep.subtractAlgorithm(15, 5);
-
-        /*
-         * Imprimir el resultado de la operación.
-         * 
-         * Finalmente, imprimimos el resultado de la operación a la consola. Esto nos
-         * permite
-         * ver el resultado de nuestro método 'subtractWithoutSubtracting'. En este
-         * ejemplo,
-         * estamos esperando que el resultado sea 10, ya que 15 menos 5 es igual a 10.
-         * Esta línea demuestra cómo podemos interactuar con los métodos de nuestras
-         * instancias
-         * y usar los resultados de sus operaciones en otras partes de nuestro código,
-         * como
-         * parte de mensajes de salida o en lógicas de decisión más complejas.
-         */
         System.out.println("El resultado de restar 15 - 5 sin usar el operador de resta es: " + resultado);
 
         //MULTIPLICACION
         resultado = dataStructuresPrep.multiplyAlgorithm(5, 3);
         System.out.println("El resultado de multiplicar 5 * 3 sin usar el operador de multiplicacion es: " + resultado);
 
-
         // DIVICION
         resultado = dataStructuresPrep.divideWAlgorithm(25, 5);
-        System.out.println("El resultado de dividir 5 / 3 sin usar el operador de division es: " + resultado);
+        System.out.println("El resultado de dividir 25 / 5 sin usar el operador de division es: " + resultado);
+
+        // NUMERO perfecto
+        int num = 28;
+        Boolean resulTBool = dataStructuresPrep.isPerfectNumber(num);
+        System.out.println("El numero "+num+(resulTBool?" es perfecto":" no es perfecto"));
+
+        // FACTORIAL
+        resultado = dataStructuresPrep.getFactorial(5);
+        System.out.println("El factorial es " + resultado);
+        //ENCONTRAR MAXIMO
+        int numeros[]={5,4,7,1,8,3,5};
+        resultado = dataStructuresPrep.findMaximum(numeros);
+        System.out.println("El numero maximos es " + resultado);
+
+        //ALGORITMO DE EXPONENCIAL
+        resultado = dataStructuresPrep.powerAlgorithm(2,3);
+        System.out.println("El exponencial es " + resultado);
+
+        //NUMEROS PRIMOS
+        num = 7;
+        resulTBool = dataStructuresPrep.isPrime(num);
+        System.out.println("El numero "+num+(resulTBool?" es primo":" no es primo"));
+        
+        //CONTAR LONGITUD
+        num = 421412414;
+        resultado = dataStructuresPrep.countDigits(num);
+        System.out.println("El numero "+num+ " contiene "+resultado+" numeros");
     }
 }
--- src/DataStructuresPrep.java
+++ src/DataStructuresPrep.java
@@ -1,6 +1,31 @@
+import java.util.ArrayList;
 
+/**
+ * La clase DataStructuresPrep está diseñada para reforzar conceptos
+ * fundamentales de programación. A través de una serie
+ * de métodos que implementan ejercicios prácticos, se puede
+ * reforzar la comprensión de operaciones básicas como aritmética sin operadores
+ * directos, verificación de números perfectos, cálculo de factoriales, entre
+ * otros.
+ * 
+ * Estos ejercicios no solo mejoran la lógica de programación y la capacidad de
+ * resolución de problemas de los estudiantes, sino que también les preparan
+ * para enfrentar los desafíos más complejos que encontrarán en el curso de
+ * Estructura de Datos. Al dominar estas habilidades fundamentales, los
+ * estudiantes
+ * estarán mejor equipados para comprender cómo operan internamente las
+ * estructuras de datos y cómo pueden manipularlas eficientemente.
+ */
 public class DataStructuresPrep {
 
+    /**
+     * Resta dos números sin utilizar el operador de resta.
+     * 
+     * @param a El minuendo.
+     * @param b El sustraendo.
+     * @return El resultado de restar b a a.
+     *         Ejemplo: subtractAlgorithm(5, 3) retorna 2.
+     */
     public int subtractAlgorithm(int a, int b) {
        int c= 0;
        while (a>(b+c)) {
@@ -10,7 +35,14 @@ public class DataStructuresPrep {
        return c;
     }
 
-
+    /**
+     * Multiplica dos números sin utilizar el operador de multiplicación.
+     * 
+     * @param a El primer factor.
+     * @param b El segundo factor.
+     * @return El producto de a y b.
+     *         Ejemplo: multiplyAlgorithm(5, 3) retorna 15.
+     */
     public int multiplyAlgorithm(int a, int b) {
         int c =0;
         while(b<=a){
@@ -20,7 +52,14 @@ public class DataStructuresPrep {
         return c;
     }
 
-
+    /**
+     * Divide dos números sin utilizar el operador de división.
+     * 
+     * @param dividend El dividendo.
+     * @param divisor  El divisor.
+     * @return El cociente de la división de dividend por divisor.
+     *         Ejemplo: divideWAlgorithm(10, 2) retorna 5.
+     */
     public int divideWAlgorithm(int dividend, int divisor) {
         int c =0;
         for(int i = 0; i<divisor;i++){
@@ -32,32 +71,98 @@ public class DataStructuresPrep {
         return c;
     }
 
+    /**
+     * Verifica si un número es perfecto. Un número es perfecto si es igual a la
+     * suma de sus divisores propios positivos, excluyéndose a sí mismo.
+     * 
+     * @param number El número a verificar.
+     * @return Verdadero si el número es perfecto, falso en caso contrario.
+     *         Ejemplo: isPerfectNumber(28) retorna true.
+     */
     public boolean isPerfectNumber(int number) {
-        throw new UnsupportedOperationException("Not yet implemented");
+        int suma = 0;
+        for (int i = 1; i < number; i++) {
+            if (number % i == 0) {
+                suma += i;
+            }
+        }
+        return suma == number;
     }
 
-
+    /**
+     * Calcula el factorial de un número.
+     * 
+     * @param number El número.
+     * @return El factorial de number.
+     *         Ejemplo: getFactorial(5) retorna 120.
+     */
     public int getFactorial(int number) {
-        throw new UnsupportedOperationException("Not yet implemented");
+        int aux=1;
+        for(int i = 1;i<=number;i++){
+            aux=aux*i;
+        }
+        return aux;
     }
 
-
+    /**
+     * Encuentra el número mayor en un arreglo de enteros.
+     * 
+     * @param numbers El arreglo de enteros.
+     * @return El valor máximo en el arreglo.
+     *         Ejemplo: findMaximum(new int[]{1, 5, 3, 6, 8, 2}) retorna 8.
+     */
     public int findMaximum(int[] numbers) {
-        throw new UnsupportedOperationException("Not yet implemented");
+        int max=0;
+        for (int num : numbers) {
+            if(num>max){
+                max = num;
+            }
+        }
+        return max;
     }
 
-
+    /**
+     * Calcula la potencia de un número sin utilizar Math.pow.
+     * 
+     * @param base     La base.
+     * @param exponent El exponente.
+     * @return El resultado de elevar la base al exponente.
+     *         Ejemplo: powerAlgorithm(2, 3) retorna 8.
+     */
     public int powerAlgorithm(int base, int exponent) {
-        throw new UnsupportedOperationException("Not yet implemented");
+        int result=base;
+        for(int i = 1;i<exponent;i++){
+            result = result*base;
+
+        }
+        return result;
     }
 
-
+    /**
+     * Verifica si un número es primo.
+     * 
+     * @param number El número a verificar.
+     * @return Verdadero si el número es primo, falso en caso contrario.
+     *         Ejemplo: isPrime(5) retorna true.
+     */
     public boolean isPrime(int number) {
-        throw new UnsupportedOperationException("Not yet implemented");
+        for(int i = 2; i <= number/2;i++){
+            if(number%i==0){
+                return true;
+            }
+        }
+        return false;
     }
 
-
+    /**
+     * Cuenta cuántos dígitos tiene un número.
+     * 
+     * @param number El número.
+     * @return La cantidad de dígitos en number.
+     *         Ejemplo: countDigits(12345) retorna 5.
+     */
     public int countDigits(int number) {
-        throw new UnsupportedOperationException("Not yet implemented");
+        String num = Integer.toString(number);
+        return num.length();
     }
 }
